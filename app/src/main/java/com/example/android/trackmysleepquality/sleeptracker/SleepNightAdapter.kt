35d923f2da38facbab49bc3083b1cb587e1d6d62
Mubanga <mubangeriseme@yahoo.com>
2019-07-21 04:23:06 +0200

    Lesson 7: Exercise 12: Refresh Data with DiffUtil

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -22,6 +22,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.TextItemViewHolder
@@ -94,6 +95,17 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
         }
 
 
+    }
+
+    class SleepNightDiffCallback : DiffUtil.ItemCallback<SleepNight>()
+    {
+        override fun areItemsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+            return oldItem.nightId==newItem.nightId
+        }
+
+        override fun areContentsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
     }
 
 
